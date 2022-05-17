2db4583bd1943f748e05114e73c81f9aa6b9f2e4
Dana <63482575+mxmarsh@users.noreply.github.com>
2022-05-17 08:49:52 -0600

    Cleaned up Github repo

---
+++ Sandbox/src/TestRectangle.java
@@ -0,0 +1,13 @@
+
+public class TestRectangle {
+
+	public static void main(String[] args) {
+
+		Rectangle rectangle1 = new Rectangle();
+
+		Rectangle rectangle2 = new Rectangle(2, 5);
+		System.out.println("The area of the new rectangle is " + rectangle2.getArea());
+
+	}
+
+}
