a7a75dc7efeb352b96b695a6718e258c5cba4358
Dana <63482575+mxmarsh@users.noreply.github.com>
2022-06-22 13:43:41 -0600

    RAF example

---
+++ Sandbox/src/mod8/StudentExercise.java
@@ -0,0 +1,22 @@
+package mod8;
+
+public class StudentExercise {
+
+	public static void main(String[] args) {
+		// create a RAF object
+
+		// set the pointer to the start of the file
+
+		// write the info for Joe Blow: name, age, GPA, active
+
+		// write the info for John Smith: name, age, GPA, active
+
+		// write the info for Jane Doe: name, age, GPA, active
+
+		// write the info for Tom Cruise: name, age, GPA, active
+
+		raf.close();
+
+	}
+
+}
