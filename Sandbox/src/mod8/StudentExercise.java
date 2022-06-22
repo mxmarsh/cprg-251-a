1063ae7bbbdc1850de3d6c5214cc360cf54277e3
Dana <63482575+mxmarsh@users.noreply.github.com>
2022-06-22 14:57:44 -0600

    Student Exercise example updated

--- Sandbox/src/mod8/StudentExercise.java
+++ Sandbox/src/mod8/StudentExercise.java
@@ -6,11 +6,12 @@ public class StudentExercise {
 
 	public static void main(String[] args) {
 
-		try {
+		final byte RECORD_LENGTH = 77 + 1 + 4 + 1;
+		// 75 chars for name, 1 byte for age, 4 bytes for GPA, 1 byte for active
 
+		try {
 			// create a RAF object
-			File f = new File("res/students.bin");
-			RandomAccessFile raf = new RandomAccessFile(f, "rw");
+			RandomAccessFile raf = new RandomAccessFile("res/students.bin", "rw");
 
 			// set the pointer to the start of the file
 			raf.seek(0);
@@ -39,6 +40,38 @@ public class StudentExercise {
 			raf.writeByte(71);
 			raf.writeFloat(1.5f);
 			raf.writeBoolean(false);
+
+			// move the pointer back to the start
+			raf.seek(0);
+
+			// for each student:
+			// read student information and print to the screen
+			System.out.println("========== PRINTING ALL STUDENTS ==========");
+			for (int counter = 0; counter < raf.length(); counter += RECORD_LENGTH) {
+				System.out.println("Student #" + (counter / RECORD_LENGTH + 1) + ":\t" + raf.readUTF().trim()
+						+ "\n\tAge:\t" + raf.readByte() + "\n\tGPA:\t" + raf.readFloat() + "\n\tActive:\t"
+						+ raf.readBoolean() + "\n");
+			}
+
+			// then, do the same, but ONLY show active students
+			raf.seek(0);
+			System.out.println("========== PRINTING ACTIVE STUDENTS ==========");
+			// for each ACTIVE student: display student info
+			for (int counter = 0; counter < raf.length(); counter += RECORD_LENGTH) {
+
+				// move the pointer to the active flag and read in its value:
+				raf.seek(counter + RECORD_LENGTH - 1);
+				boolean isActive = raf.readBoolean(); // now the pointer is at the end of this record
+
+				if (isActive) {
+					// move the pointer back to the start of the record:
+					raf.seek(counter);
+
+					System.out.println("Student #" + (counter / RECORD_LENGTH + 1) + ":\t" + raf.readUTF().trim()
+							+ "\n\tAge:\t" + raf.readByte() + "\n\tGPA:\t" + raf.readFloat() + "\n\tActive:\t"
+							+ raf.readBoolean() + "\n"); // now the pointer is at the end of this record
+				} // end if
+			} // end for
 
 			raf.close();
 
